3116f8a36e36ca3912268fbc038462749f63d8e7
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-08-29 14:46:41 -0300

    spring security starter

---
+++ src/main/java/com/lf2a/springsecurity/demo/config/SecurityWebApplicationWebInitializer.java
@@ -0,0 +1,6 @@
+package com.lf2a.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
