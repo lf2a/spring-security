f57999418c699df6a03c66c2628735689308fb38
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-08-29 13:49:12 -0300

    first commit

---
+++ src/main/java/com/lf2a/springsecurity/demo/controller/DemoConrtoller.java
@@ -0,0 +1,12 @@
+package com.lf2a.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoConrtoller {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
